a6479a1a5981a48ea304d9733849548a7a097635
Alan <33831091+Doyers@users.noreply.github.com>
2018-08-24 15:26:31 +0200

    Character start for platformer
    The player is able to move left, right, up and crouch under obsticles. The player can also be able to teleport to 'checkpoints when he goes out of bonds.
    TODO
    - Jump animation fix
    - Multiple checkpoints that can change and save.

---
+++ GameMechanics/Assets/Doyers_Production/Scripts/OutOfBounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour {
+
+    public GameObject[] checkpoints;
+    private Vector3 currentCheckpoint;
+
+
+	// Use this for initialization
+	void Start () {
+
+        if (checkpoints[0] == null)
+            checkpoints[0].transform.position = new Vector3(0, 0, 0);
+
+        currentCheckpoint = checkpoints[0].transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        ///TODO
+        ///if player gets to new checkpoint EN is groter dan de vorige checkpoint, maar dat nieuwe checkpoint
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.position = currentCheckpoint;
+            Debug.Log("player collision");
+        }
+    }
+}
